88eb4d3e7c62ea6ca3f688af62f8567646a49bd9
Alejandro <alejandroarraga99@gmail.com>
2025-12-19 19:23:54 -0300

    Inicial commit: Estructura inicial de la web

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: ['./*.html'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#f5b300',
+                secondary: '#aa7700',
+                accent: '#042c78',
+                gray: '#333333',
+                lightgray: '#dddddd',
+            },
+            fontFamily: {
+                sans: ['Poppins', 'sans-serif'],
+                heading: ['Jost', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
